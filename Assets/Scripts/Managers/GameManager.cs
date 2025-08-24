e7013dfc690db38404274f9ff5de2b93ed076484
cbarria <cbarria@yahoo.com>
2025-08-24 02:13:57 -0400

    Lower background music volume to 0.1f

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -97,7 +97,7 @@ public class GameManager : MonoBehaviour
         {
             bgMusic.clip = bgClip;
             bgMusic.Play();
-            bgMusic.volume = 0.2f; // Soft volume as requested
+            bgMusic.volume = 0.1f; // Halved for softer background
         }
         else
         {
